6d52b0e02df49b15797b9496dd0c78ae67956fa6
Mahfuz Alam <mahfuz.alam.shohan@gmail.com>
2026-01-06 20:30:46 +0600

    Create subjectGroups.ts

---
+++ src/frontend/components/public/subjectGroups.ts
@@ -0,0 +1,95 @@
+export const subjectGroups =`
+const subjectGroups = {
+            SSC: {
+                Science: [
+                    'Bangla 1st Paper',
+                    'Bangla 2nd Paper',
+                    'English 1st Paper',
+                    'English 2nd Paper',
+                    'General Mathematics',
+                    'Physics',
+                    'Chemistry',
+                    'Biology',
+                    'Higher Mathematics',
+                    'Bangladesh and Global Studies',
+                    'Information and Communication Technology',
+                    'Religion and Moral Education'
+                ],
+                Humanities: [
+                    'Bangla 1st Paper',
+                    'Bangla 2nd Paper',
+                    'English 1st Paper',
+                    'English 2nd Paper',
+                    'General Mathematics',
+                    'Bangladesh and Global Studies',
+                    'Information and Communication Technology',
+                    'Geography and Environment',
+                    'History of Bangladesh and World Civilization',
+                    'Civics and Citizenship',
+                    'Religion and Moral Education'
+                ],
+                'Business Studies': [
+                    'Bangla 1st Paper',
+                    'Bangla 2nd Paper',
+                    'English 1st Paper',
+                    'English 2nd Paper',
+                    'General Mathematics',
+                    'Bangladesh and Global Studies',
+                    'Information and Communication Technology',
+                    'Accounting',
+                    'Business Entrepreneurship',
+                    'Finance and Banking',
+                    'Religion and Moral Education'
+                ]
+            },
+            HSC: {
+                Science: [
+                    'Bangla 1st Paper',
+                    'Bangla 2nd Paper',
+                    'English 1st Paper',
+                    'English 2nd Paper',
+                    'Information and Communication Technology',
+                    'Physics 1st Paper',
+                    'Physics 2nd Paper',
+                    'Chemistry 1st Paper',
+                    'Chemistry 2nd Paper',
+                    'Biology 1st Paper',
+                    'Biology 2nd Paper',
+                    'Higher Mathematics 1st Paper',
+                    'Higher Mathematics 2nd Paper'
+                ],
+                Humanities: [
+                    'Bangla 1st Paper',
+                    'Bangla 2nd Paper',
+                    'English 1st Paper',
+                    'English 2nd Paper',
+                    'Information and Communication Technology',
+                    'Economics 1st Paper',
+                    'Economics 2nd Paper',
+                    'History 1st Paper',
+                    'History 2nd Paper',
+                    'Civics and Good Governance 1st Paper',
+                    'Civics and Good Governance 2nd Paper',
+                    'Logic 1st Paper',
+                    'Logic 2nd Paper'
+                ],
+                'Business Studies': [
+                    'Bangla 1st Paper',
+                    'Bangla 2nd Paper',
+                    'English 1st Paper',
+                    'English 2nd Paper',
+                    'Information and Communication Technology',
+                    'Accounting 1st Paper',
+                    'Accounting 2nd Paper',
+                    'Business Organization and Management 1st Paper',
+                    'Business Organization and Management 2nd Paper',
+                    'Finance, Banking and Insurance 1st Paper',
+                    'Finance, Banking and Insurance 2nd Paper',
+                    'Production Management and Marketing 1st Paper',
+                    'Production Management and Marketing 2nd Paper'
+                ]
+            }
+        };
+
+`;
+
