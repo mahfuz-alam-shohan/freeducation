87a00e7b5bf68eef8f290191b809d776e010fc8a
Mahfuz Alam <mahfuz.alam.shohan@gmail.com>
2026-01-06 21:47:51 +0600

    Create scienceConfigs.ts

---
+++ src/frontend/components/public/scienceConfigs.ts
@@ -0,0 +1,83 @@
+export const scienceConfigs =`
+                const scienceConfigs = [
+                    {
+                        classLabel: 'SSC',
+                        subjectLabel: 'Physics',
+                        chapters: sscPhysicsChapters,
+                        listRoute: 'public-ssc-physics-topics',
+                        topicRoute: 'public-ssc-physics-topic'
+                    },
+                    {
+                        classLabel: 'SSC',
+                        subjectLabel: 'Chemistry',
+                        chapters: sscChemistryChapters,
+                        listRoute: 'public-ssc-chemistry-topics',
+                        topicRoute: 'public-ssc-chemistry-topic'
+                    },
+                    {
+                        classLabel: 'SSC',
+                        subjectLabel: 'Biology',
+                        chapters: sscBiologyChapters,
+                        listRoute: 'public-ssc-biology-topics',
+                        topicRoute: 'public-ssc-biology-topic'
+                    },
+                    {
+                        classLabel: 'SSC',
+                        subjectLabel: 'Bangladesh and Global Studies',
+                        chapters: sscBangladeshGlobalChapters,
+                        listRoute: 'public-ssc-bangladesh-global-studies-topics',
+                        topicRoute: 'public-ssc-bangladesh-global-studies-topic'
+                    },
+                    {
+                        classLabel: 'HSC',
+                        subjectLabel: 'Physics 1st Paper',
+                        chapters: hscPhysics1stChapters,
+                        listRoute: 'public-hsc-physics-1st-topics',
+                        topicRoute: 'public-hsc-physics-1st-topic'
+                    },
+                    {
+                        classLabel: 'HSC',
+                        subjectLabel: 'Physics 2nd Paper',
+                        chapters: hscPhysics2ndChapters,
+                        listRoute: 'public-hsc-physics-2nd-topics',
+                        topicRoute: 'public-hsc-physics-2nd-topic'
+                    },
+                    {
+                        classLabel: 'HSC',
+                        subjectLabel: 'Chemistry 1st Paper',
+                        chapters: hscChemistry1stChapters,
+                        listRoute: 'public-hsc-chemistry-1st-topics',
+                        topicRoute: 'public-hsc-chemistry-1st-topic'
+                    },
+                    {
+                        classLabel: 'HSC',
+                        subjectLabel: 'Chemistry 2nd Paper',
+                        chapters: hscChemistry2ndChapters,
+                        listRoute: 'public-hsc-chemistry-2nd-topics',
+                        topicRoute: 'public-hsc-chemistry-2nd-topic'
+                    },
+                    {
+                        classLabel: 'HSC',
+                        subjectLabel: 'Biology 1st Paper',
+                        chapters: hscBiology1stChapters,
+                        listRoute: 'public-hsc-biology-1st-topics',
+                        topicRoute: 'public-hsc-biology-1st-topic'
+                    },
+                    {
+                        classLabel: 'HSC',
+                        subjectLabel: 'Biology 2nd Paper',
+                        chapters: hscBiology2ndChapters,
+                        listRoute: 'public-hsc-biology-2nd-topics',
+                        topicRoute: 'public-hsc-biology-2nd-topic'
+                    },
+                    {
+                        classLabel: 'HSC',
+                        subjectLabel: 'Information and Communication Technology',
+                        chapters: hscIctChapters,
+                        listRoute: 'public-hsc-ict-topics',
+                        topicRoute: 'public-hsc-ict-topic',
+                        questionKey: 'ICT'
+                    }
+                ];
+
+`;
