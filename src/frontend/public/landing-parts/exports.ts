a5a6aa7ba6fc53ae05ffe8680d06f1db157cb20f
Mahfuz Alam <mahfuz.alam.shohan@gmail.com>
2026-01-07 06:43:57 +0600

    Create exports.ts

---
+++ src/frontend/public/landing-parts/exports.ts
@@ -0,0 +1,41 @@
+export const landingExports = `
+        return {
+            StudentLanding,
+            SubjectIndexPage,
+            PublicBanglaShell,
+            PublicBanglaTopicGrid,
+            PublicBanglaTextList,
+            PublicBanglaShohopathList,
+            PublicBanglaItemDetail,
+            PublicBanglaSrijonshilDetail,
+            PublicBanglaMcqDetail,
+            PublicIctShell,
+            PublicIctChapterList,
+            PublicIctMcqDetail,
+            PublicScienceShell,
+            PublicScienceChapterList,
+            PublicScienceTopicList,
+            PublicScienceTopicDetail,
+            PublicScienceCqDetail,
+            PublicScienceMcqDetail,
+            PublicVideoList,
+            PublicVideoDetail,
+            PublicReligionOptionList,
+            PublicEnglishShell,
+            PublicEnglishCardGrid,
+            PublicEnglishTypeList,
+            PublicEnglishQuestionList,
+            sscSubjects,
+            hscSubjects,
+            religionOptions
+        };
+        })();
+        const {
+            StudentLanding, SubjectIndexPage, PublicBanglaShell, PublicBanglaTopicGrid, PublicBanglaTextList,
+            PublicBanglaShohopathList, PublicBanglaItemDetail, PublicBanglaSrijonshilDetail, PublicBanglaMcqDetail,
+            PublicIctShell, PublicIctChapterList, PublicIctMcqDetail, PublicScienceShell, PublicScienceChapterList,
+            PublicScienceTopicList, PublicScienceTopicDetail, PublicScienceCqDetail, PublicScienceMcqDetail,
+            PublicVideoList, PublicVideoDetail, PublicReligionOptionList, PublicEnglishShell, PublicEnglishCardGrid,
+            PublicEnglishTypeList, PublicEnglishQuestionList, sscSubjects, hscSubjects, religionOptions
+        } = LandingModule;
+`;
