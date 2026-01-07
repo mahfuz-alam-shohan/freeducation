c5c8fd30f8b2d63854a1fe7f40b71670547e1a66
Mahfuz Alam <mahfuz.alam.shohan@gmail.com>
2026-01-07 10:10:42 +0600

    Create exports.ts

---
+++ src/frontend/admin/dashboard-parts/exports.ts
@@ -0,0 +1,25 @@
+export const dashboardExports = `
+        return {
+            TeacherDashboard,
+            AdminDashboard,
+            AdminGroupSelection,
+            AdminGroupDetail,
+            BanglaFirstPaperTopics,
+            BanglaShahitto,
+            BanglaShohopath,
+            BanglaTextList,
+            BanglaItemDetail,
+            SrijonshilTypeList,
+            SrijonshilQuestionList,
+            McqQuestionList,
+            IctChapterList,
+            ReligionSelectionList,
+            ScienceChapterList,
+            ScienceTopicList,
+            ScienceTopicDetail,
+            VideoManager,
+            EnglishFirstPaperHome,
+            EnglishSectionList,
+            EnglishQuestionList
+        };
+`;
